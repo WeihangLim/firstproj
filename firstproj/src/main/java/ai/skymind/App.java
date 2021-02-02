b1c375e1877e068f363ddb3fa8d6bc03a220c900
WeihangLim <77949809+WeihangLim@users.noreply.github.com>
2021-02-02 12:11:56 +0800

    Add files via upload

---
+++ firstproj/src/main/java/ai/skymind/App.java
@@ -0,0 +1,16 @@
+package ai.skymind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{ public App(){
+
+}
+    public static void main( String[] args )
+
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ firstproj/src/main/java/ai/skymind/firstprog.java
@@ -0,0 +1,7 @@
+package ai.skymind;
+
+public class firstprog {
+    public static void main(String[] args) {
+        System.out.println("first program run successfully");
+    }
+}
